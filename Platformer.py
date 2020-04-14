e9a79c98e50cb104657862d92ff308d05f7cb0b3
Scripter17 <jameschristopherwise@gmail.com>
2020-04-14 15:34:18 -0400

    Testing to make sure the game works on my Windows Ubuntu Subsystem

--- Platformer.py
+++ Platformer.py
@@ -1,76 +1,87 @@
+#    Platformer.py v1.0.0
+# James C. Wise - 2020-04-14 
+#  See LICENSE.md for legal
+
 import colorama, os, keyboard, time, ctypes
 colorama.init()
 
-class _CursorInfo(ctypes.Structure):
-	_fields_ = [("size", ctypes.c_int),
-	("visible", ctypes.c_byte)]
-ci = _CursorInfo()
-handle = ctypes.windll.kernel32.GetStdHandle(-11)
-ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
-ci.visible = False
-ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
+# Some ctypes bullshit to make the terminal cursor not appear
+try:
+	class _CursorInfo(ctypes.Structure):
+		_fields_ = [("size", ctypes.c_int),
+		("visible", ctypes.c_byte)]
+	ci = _CursorInfo()
+	handle = ctypes.windll.kernel32.GetStdHandle(-11)
+	ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
+	ci.visible = False
+	ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
+except:
+	pass
+# Good lord that was gross
 
-cols, lines=50, 31
-os.system("mode con: cols="+str(cols)+" lines="+str(lines))
 px, py=0, 0
 
+# Get map data
 mapText=open("map.txt", "r").read()
+# Not too sure why I'm doing it like `[["Block"]]` instead of `["Blocks"]` but it doesn't matter too much to me
 mapData=[]
-for yi,y in enumerate(mapText.splitlines()):
+for yi, y in enumerate(mapText.splitlines()):
 	mapLine=[]
-	for xi,x in enumerate(y):
-		if x=="@":
+	for xi, x in enumerate(y):
+		if x=="@": # Set spawnpoint
 			mapLine.append(" ")
 			py, px=yi, xi
 		else:
 			mapLine.append(x)
 	mapData.append(mapLine)
-#print(len(mapData)); exit()
+width=max(*[len(x) for x in mapData])
+# Pad lines that don't extend to the bounding box
+for yi,y in enumerate(mapData):
+	mapData[yi]+=[" "]*(width-len(y))
+#os.system("mode con: cols="+str(width)+" lines="+str(len(mapData)+1)) # Set screen size to map size
 
 def getButtons():
 	buttons=""
-	if keyboard.is_pressed("up")   : buttons+="U"
-	if keyboard.is_pressed("right"): buttons+="R"
-	if keyboard.is_pressed("down") : buttons+="D"
-	if keyboard.is_pressed("left") : buttons+="L"
-	if keyboard.is_pressed("space"): buttons+="S"
+	if keyboard.is_pressed("up")     : buttons+="U" # Phase up 3 blocks
+	if keyboard.is_pressed("right")  : buttons+="R" # Move right
+	if keyboard.is_pressed("down")   : buttons+="D" # Stop falling
+	if keyboard.is_pressed("left")   : buttons+="L" # Move right
+	if keyboard.is_pressed("space")  : buttons+="S" # Phase up 5 blocks
+	if keyboard.is_pressed("control"): buttons+="+" # CTRL+Right / CTRL+Left phase sidewats 2 blocks
 	return buttons
 
-fallTimer=time.time()
+fallTimer=time.time() # You only fall every 0.5 seconds (Well it's every 0.6 because the vFPS is 0.2 but whatever)
 buttons="."
-firstFrame=True
 while True:
 	# Handle buttons
 	# Print screen
-	if buttons!="":
+	if buttons!="": # If there's no player input, don't reprint the map
 		for yi, y in enumerate(mapData):
 			for xi, x in enumerate(mapData[yi]):
 				print("\033["+str(yi+1)+";"+str(xi+1)+"H"+x)
-		firstFrame=False
 	# Calculate death/win conditions 
 	if py==len(mapData)-1 or mapData[py][px]=="X":
-		os.system("cls")
-		print("Dead")
-		os.system("pause")
+		print("\033[2J\033[1;1HDead")
+		input("Press Enter to continue")
 		exit()
 	elif mapData[py][px]=="O":
-		os.system("cls")
-		print("Win")
-		os.system("pause")
+		print("\033[2J\033[1;1HWin")
+		input("Press Enter to continue")
 		exit()
 	# Erase player
+	# If there's no player movement but you're falling, this prevents a trail from happening
 	print("\033["+str(py+1)+";"+str(px+1)+"H"+mapData[py][px])
 	# Compute next position
-	if "R" in buttons and px+1<len(mapData[py]) and mapData[py][px+1]!="#": px+=1
-	if "L" in buttons and px-1>=0 and mapData[py][px-1]!="#": px-=1
+	hphase=1+int("+" in buttons)
+	if "R" in buttons and px+hphase<len(mapData[py]) and mapData[py][px+hphase]!="#"   : px+=hphase
+	if "L" in buttons and px-hphase>=0 and mapData[py][px-hphase]!="#"                 : px-=hphase
 	if "U" in buttons and py-3>=0 and mapData[py+1][px]=="#" and mapData[py-3][px]!="#": py-=3
 	if "S" in buttons and py-5>=0 and mapData[py+1][px]=="#" and mapData[py-5][px]!="#": py-=5
-	if not ("D" in buttons) and mapData[py+1][px]!="#" and time.time()-fallTimer>0.5:
-		py+=1
-		fallTimer=time.time()
-	# Print player
-	print("\033["+str(py+1)+";"+str(px+1)+"H"+"@")
+	if not ("D" in buttons) and mapData[py+1][px]!="#" and time.time()-fallTimer>0.5   : py+=1; fallTimer=time.time()
+	print("\033["+str(py+1)+";"+str(px+1)+"H"+"@") # Print player
 	buttons=""
 	for i in range(12):
+		# Checks for buttons every 60th of a second but only does physics every 5th of a second
+		# It is a very stupid solution but it works
 		buttons+=getButtons()
 		time.sleep(1/60)
--- README.txt
+++ README.txt
@@ -8,11 +8,13 @@ Requirements:
 	- Tolerance for unpolished games
 
 Controls:
-	Right: Move right
-	Left:  Move left
-	Down:  Stop falling
-	Up:    Phase up 3 blocks
-	Space: Phase up 5 blocks
+	Right : Move right
+	Left  : Move left
+	Down  : Stop falling
+	Up    : Phase up 3 blocks
+	Space : Phase up 5 blocks
+	CTRL+Right : Phase 2 blocks right
+	CTRL+Left  : Phase 2 blocks left
 
 Phasing:
 	Unlike in most platformers which have jumps that are stopped by ceilings, this game has phasing
@@ -20,20 +22,26 @@ Phasing:
 	If there's a block 4 blocks above you when you hit space, instead of being blocked by the ceiling and only going up 3 blocks, you'll still go up 5 blocks; Effectively phasing through the block
 
 Making maps:
-	`map.txt` is the map file; In it, there are 5 "tile" types:
+	`map.txt` is the map file; In it, there are 5 tile types:
 	Air   ( ): No special effect
-	Block (#): Blocks movement/falling and phasing; You can only phase while standing on a block
+	Block (#): Blocks movement/falling and phasing; You can only vertically phase while standing on a block
 	Death (X): When touched, the player dies
 	Win   (O): When touched, the player wins
 	Spawn (@): Defines where the player spawns from
 
 TODO/Known Bugs:
-	- Shift+Right / Shift+Left allowing you to phase through walls
 	- Map select screen
-	- Attempting to enter a tile that isn't in map.txt-even if it's within the bounding box for the map-crashes the game
-	- Better documentation
+	- Better code documentation
 	- Compile game into an exe
+	- Colors?
+	- Check if it works on Linux (Or at least Ubuntu)
 
 Changelog:
 v1.0.0 - Initial:
 	- Created the game
+v1.1.0 - Sideways phasing:
+	- Control+Right and Control+Left allows you to phase 2 blocks to the right and left
+	- Fixed entering an undefined tile within the map's bounding box crashing the game
+	  The default map.txt reflects this on line 8
+	- The game window now automatically resizes into the map's dimensions
+	  Whether or not this change will stay is to be determined
